0cf009931117781084fe29b64d8537c03f6858a5
Hannington Omondi <omondihannington12@gmail.com>
2024-09-20 18:42:46 +0300

    addin interfacce settings for render

--- frontend/healthNfitness/vite.config.js
+++ frontend/healthNfitness/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Bind to all available interfaces
+    port: 3000        // Make sure the port is set as required by Render
+  }
 })
